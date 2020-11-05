89081ff8ee0ee2cf1fb4c9985049e642095861a1
maitreebain <maitreebain@gmail.com>
2020-11-05 16:10:17 -0500

    working on create contact

---
+++ ContactsV2/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  ContactsV2
+//
+//  Created by Maitree Bain on 11/5/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+struct Contact: Hashable, Equatable {
+    let first: String
+    let last: String
+    let phone: Int
+    let photo: UIImage
+    let email: String
+}
---
+++ ContactsV2/ContactsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsV2
+//
+//  Created by Maitree Bain on 11/5/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
